f02f14ac3534fb4cd5d631dee2968c5ca3b8afcb
log.apps2020@gmail.com <logapps2015>
2020-06-17 13:54:25 +0200

    details

---
+++ app/src/main/java/com/logapps/tourism_app/Suggest_details.java
@@ -0,0 +1,14 @@
+package com.logapps.tourism_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Suggest_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_suggest_details);
+    }
+}
